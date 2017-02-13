7d53c3cb3debfc321c267e87e91ac7c53b8fccb1
MeatballMan <david.wilsby@gmail.com>
2017-02-13 21:44:50 +0100

    Created Pokemon Class

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by David Wilsby on 2017-02-13.
+//  Copyright © 2017 David Wilsby. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
