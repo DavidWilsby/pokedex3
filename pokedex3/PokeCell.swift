fa56791cd183b6227cd65a7eb3dd3636ba482706
MeatballMan <david.wilsby@gmail.com>
2017-02-13 22:22:23 +0100

    PokeCell
    Created a custom collection view cell “PokeCell” and connected the
    IBOutlets.

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by David Wilsby on 2017-02-13.
+//  Copyright © 2017 David Wilsby. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
