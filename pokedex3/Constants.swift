5cc72bb1a1eb0f3cdae6057b4525392e9ba3ce55
MeatballMan <david.wilsby@gmail.com>
2017-03-30 00:24:03 +0200

    Added PodFile and set up Downloading
    Created the Alamofire podfile and set up the code for downloading all
    the data from the pokeApi

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by David Wilsby on 2017-03-29.
+//  Copyright © 2017 David Wilsby. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.com"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> () 
--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -25,18 +25,40 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
     
-    @IBAction func backBtnPressed(_ sender: UIButton) {
-        
-        dismiss(animated: true, completion: nil)
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLbl.text = pokemon.name
+        nameLbl.text = pokemon.name.capitalized
         
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        
+        pokemon.downloadPokemonDetail {
+            
+            //Whatever we write here will only be called after the network call is complete!
+            self.updateUI()
+            
+        }
+
+    }
+    
+    func updateUI() {
+        
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
     }
 
 }
